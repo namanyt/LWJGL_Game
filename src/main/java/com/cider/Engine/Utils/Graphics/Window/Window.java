7f442f0c45df2da0acc71d6ebb43cb47311bdd6a
namanyt <namanyt1111@gmail.com>
2021-08-24 16:30:04 +0530

    Added Controls Manager

--- src/main/java/com/cider/Engine/Utils/Graphics/Window/Window.java
+++ src/main/java/com/cider/Engine/Utils/Graphics/Window/Window.java
@@ -1,7 +1,7 @@
 package com.cider.Engine.Utils.Graphics.Window;
 
 import com.cider.Engine.Errors.LWJGL_Error;
-import org.lwjgl.Version;
+import com.cider.Engine.Utils.Controls.KeyListener;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
@@ -16,79 +16,92 @@ import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Window {
-    private long window;
-    int width, height;
-    String title;
-    long monitor, share;
+  private long window;
+  int width, height;
+  String title;
+  long monitor, share;
 
-    public Window(int width, int height, String title, long monitor, long share) {
-        this.width = width;
-        this.height = height;
-        this.title = title;
-        this.monitor = monitor;
-        this.share = share;
+  public Window(int width, int height, String title, long monitor, long share) {
+    this.width = width;
+    this.height = height;
+    this.title = title;
+    this.monitor = monitor;
+    this.share = share;
+  }
+
+  public void stop() {
+    glfwFreeCallbacks(window);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    glfwSetErrorCallback(null).free();
+  }
+
+  public void init() {
+    GLFWErrorCallback.createPrint(System.err).set();
+    if (!glfwInit())
+      try {
+        throw new LWJGL_Error("Unable to initialize GLFW");
+      } catch (LWJGL_Error e) {
+        e.printStackTrace();
+      }
+
+    glfwDefaultWindowHints();
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    window = glfwCreateWindow(this.width, this.height, this.title, this.monitor, this.share);
+    if (window == NULL)
+      throw new RuntimeException("Failed to create the GLFW window");
+
+    setKeyCallbacks();
+
+    ManageMemoryAndCenterWindow();
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    glfwShowWindow(window);
+    GL.createCapabilities();
+  }
+
+  public void setKeyCallbacks() {
+    glfwSetCursorPosCallback    (window, com.cider.Engine.Utils.Controls.MouseListener::mousePosCallback);
+    glfwSetMouseButtonCallback  (window, com.cider.Engine.Utils.Controls.MouseListener::mouseButtonCallback);
+    glfwSetScrollCallback       (window, com.cider.Engine.Utils.Controls.MouseListener::mouseScrollCallback);
+    glfwSetKeyCallback          (window, com.cider.Engine.Utils.Controls.KeyListener::keyCallback);
+  }
+
+  public void loop() {
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+
+    if (KeyListener.isKeyPressed(GLFW_KEY_ESCAPE)) {
+      glfwSetWindowShouldClose(window, true);
     }
 
-    public void stop() {
-        glfwFreeCallbacks(window);
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glfwSwapBuffers(window);
+
+    glfwPollEvents();
+  }
+
+  private void ManageMemoryAndCenterWindow() {
+    try (MemoryStack stack = stackPush()) {
+      IntBuffer pWidth = stack.mallocInt(1); // int*
+      IntBuffer pHeight = stack.mallocInt(1); // int*
+
+      glfwGetWindowSize(window, pWidth, pHeight);
+
+      GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+      glfwSetWindowPos(
+              window,
+              (vidmode.width() - pWidth.get(0)) / 2,
+              (vidmode.height() - pHeight.get(0)) / 2
+      );
     }
+  }
 
-    public void init() {
-            GLFWErrorCallback.createPrint(System.err).set();
-        if (!glfwInit())
-            try {
-                throw new LWJGL_Error("Unable to initialize GLFW");
-            } catch (LWJGL_Error e) {
-                e.printStackTrace();
-            }
-
-        glfwDefaultWindowHints();
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-
-        window = glfwCreateWindow(this.width, this.height, this.title, this.monitor, this.share);
-        if (window == NULL)
-            throw new RuntimeException("Failed to create the GLFW window");
-
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-                glfwSetWindowShouldClose(window, true);
-        });
-
-        try (MemoryStack stack = stackPush()) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
-
-            glfwGetWindowSize(window, pWidth, pHeight);
-
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
-        }
-
-        glfwMakeContextCurrent(window);
-        glfwSwapInterval(1);
-        glfwShowWindow(window);
-        GL.createCapabilities();
-    }
-
-    public void loop() {
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glfwSwapBuffers(window);
-
-        glfwPollEvents();
-    }
-
-    public long getWindow() {
-        return window;
-    }
+  public long getWindow() {
+    return window;
+  }
 }
